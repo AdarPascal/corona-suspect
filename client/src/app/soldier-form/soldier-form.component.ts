01a95d6928c9a153b1d5a6266df62b7c64099342
Yagor Kryshniov <egork@aplit-soft.com>
2020-07-19 17:21:16 +0300

    material

--- client/src/app/soldier-form/soldier-form.component.ts
+++ client/src/app/soldier-form/soldier-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SoldierService, ISoldierPage } from './soldier.service';
 
 @Component({
   selector: 'app-soldier-form',
@@ -8,25 +9,29 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SoldierFormComponent implements OnInit {
   profileForm = new FormGroup({
-    idnumber: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    TimeOfPositioning: new FormControl(''),
-    PlaceOfPositioning: new FormControl(''),
-    HasDeclaredInocense: new FormControl(''),
-    WasInBaseDuringPos: new FormControl(''),
-    WasSentToConfinment: new FormControl('')
+    idnumber: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.required]),
+    timeOfPositioning: new FormControl('', [Validators.required]),
+    placeOfPositioning: new FormControl('', [Validators.required]),
+    hasDeclaredInocense: new FormControl(null, [Validators.required]),
+    wasInBaseDuringPos: new FormControl(null, [Validators.required]),
+    wasSentToConfinment: new FormControl(null, [Validators.required]),
+    firstNameCommander: new FormControl('', [Validators.required]),
+    lastNameCommander: new FormControl('', [Validators.required]),
+    phoneNumberCommander: new FormControl('', [Validators.required]),
   });
 
-  constructor() { }
+  constructor(public soldierService: SoldierService) { }
 
   ngOnInit(): void {
   }
 
-  // onSubmit() {
-  //   // TODO: Use EventEmitter with form value
-  //   console.log(this.profileForm.value);
-  // }
+
+  onSend() {
+    this.soldierService.create(this.profileForm.value as ISoldierPage);
+    this.profileForm.reset();
+  }
 
 }
